35ad2f0dc2173f784c09897c26e87aa0f95a2d12
corn-flour <26082352+corn-flour@users.noreply.github.com>
2023-06-04 13:32:54 -0700

    auto filter when typing

--- src/app/(index)/search-input.tsx
+++ src/app/(index)/search-input.tsx
@@ -1,4 +1,27 @@
+"use client"
+
+import { usePathname, useRouter } from "next/navigation"
+import { useTransition } from "react"
+import { useDebouncedCallback } from "use-debounce"
+
 const SearchInput = () => {
+    const router = useRouter()
+    const [_, startTransition] = useTransition()
+    const pathname = usePathname()
+
+    const updateQueryParams = useDebouncedCallback((v: string) => {
+        const params = new URLSearchParams(window.location.search)
+        if (v) {
+            params.set("name", v)
+        } else {
+            params.delete("name")
+        }
+
+        startTransition(() => {
+            router.replace(`${pathname}?${params.toString()}`)
+        })
+    }, 300)
+
     return (
         <form>
             <label className="mx-auto my-4">
@@ -8,6 +31,7 @@ const SearchInput = () => {
                     name="name"
                     placeholder="Search.."
                     className="rounded-full bg-white px-4 py-2"
+                    onChange={(e) => updateQueryParams(e.target.value)}
                 />
             </label>
         </form>
